5a8e251126ade910f6344cf36219ca95f09a1a0f
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-04-19 11:57:37 -0500

    Add util func to get available AWS profile names

---
+++ util.py
@@ -0,0 +1,8 @@
+import boto3
+
+from typing import Iterator
+
+def aws_profiles() -> Iterator[str]:
+    """Yield all profile names that can be found in the current config file."""
+    s = boto3.Session()
+    yield from s.available_profiles
