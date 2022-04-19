58b841af091335596146c372641a61d80ef8e0bd
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-04-19 12:10:12 -0500

    Add a script to look up AWS access keys

---
+++ aws-get-access-keys.py
@@ -0,0 +1,24 @@
+import argparse
+import boto3
+import botocore.exceptions
+import notch
+import util
+
+parser = argparse.ArgumentParser(description='Find access keys for an AWS user account in all available profiles')
+parser.add_argument('username', help='The username to use when searching for access keys')
+args = parser.parse_args()
+
+log = notch.make_log('aws_get_access_keys')
+
+for profile_name in util.aws_profiles():
+    log.info(f'Using profile {profile_name}')
+    s = boto3.Session(profile_name=profile_name)
+    iam = s.resource('iam')
+    user = iam.User(args.username)
+    try:
+        log.info(f'Looking up access keys for {user.arn}')
+    except botocore.exceptions.ClientError as e:
+        log.error(e)
+        continue
+    for key in user.access_keys.all():
+        log.info(f'Found access key with id {key.id}')
